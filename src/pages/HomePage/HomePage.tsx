bdcf72ee4143393abdf5ede014bfbe92e65cc13d
SatomiJin <van.bui180903@gmail.com>
2026-02-08 23:54:46 +0700

    fix: resolve build errorsv2

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,5 @@
 import './HomePage.scss'
-import DUOTINMOBILE from '../../Assets/DUOTINMOBILE.jpg'
-import DUOTINPC from '../../Assets/DUOYIN.png'
+
 const HomePage = () => {
     return (
         <div className="home_page_container">
